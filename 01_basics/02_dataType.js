9088f9a2f4c7bc67250ce59075f517a44b147676
ameermaviaamkj-create <ameermaviaamkj@gmail.com>
2025-08-20 12:06:59 +0000

    data type

--- 01_basics/02_dataType.js
+++ 01_basics/02_dataType.js
@@ -1,1 +1,27 @@
- 
+"use strict";//treat all JS code as newer version
+
+// alert(3+3); //we are using node js, not browser
+
+console.log(3+3)//code readability should be high
+
+console.log("Ameer Mavia");
+
+
+let name="Abgeen Ameer"
+let age=16
+let isLoggedIn = false
+
+
+                           //Premitive Data Type
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+
+                            // Object
+                            
