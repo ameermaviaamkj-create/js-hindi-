550c310699855a6407444dd47b884708596927ed
ameermaviaamkj-create <ameermaviaamkj@gmail.com>
2025-08-20 10:39:46 +0000

    basisc

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,11 @@
+const accountId = 144553;
+let accountEmail = "ameermavia@gmail.com";
+var accountPassward = "12345";
+accountCity = "Shujaabad";
+let accountState;
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassward,accountCity,accountState])
+
+
