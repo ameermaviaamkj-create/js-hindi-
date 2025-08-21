9f3c4bf62241f5a433b81b949ee50ed4eb6a3a81
ameermaviaamkj-create <ameermaviaamkj@gmail.com>
2025-08-21 08:10:36 +0000

    datatype_summery

---
+++ 01_basics/datatype_summery.js
@@ -0,0 +1,35 @@
+// premitive data type
+
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false 
+const outsideTemp = null
+ let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 72364923746982n
+
+
+
+// Reference datatype(Non premetive)
+
+// Array, objects, Functions
+
+const heros = ["Ameer Mavia","Abgeen Ameer","Ameer Son"]
+let myObj = {
+    name: "Ameer mavia",
+    age:19,
+}
+
+const myFunction = function(){
+    console.log("Abgeen Ameer");
+}
+
+console.log(typeof myFunction);
