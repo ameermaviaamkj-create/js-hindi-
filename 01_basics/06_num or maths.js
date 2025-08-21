c6a5bc1cf21c2f514e67d763249f7473fa6e0e15
ameermaviaamkj-create <ameermaviaamkj@gmail.com>
2025-08-21 14:14:40 +0000

    math and nom

---
+++ 01_basics/06_num or maths.js
@@ -0,0 +1,34 @@
+const score = 400
+//console.log(score);
+
+const balance = new Number(100)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2));
+
+const otherNumber = 143.8966
+
+//console.log(otherNumber.toPrecision(5));
+
+const hundreds = 1000000
+//console.log(hundreds.toLocaleString());
+
+//+++++++++++++++++ Maths +++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));//it change number neg into positive
+// console.log(Math.round(4.6));//it is to using for round off the numbers
+// console.log(Math.ceil(4.2))// is mn ger deimal ke baad koi bhi no aa jaee to ye round off kar ke next no shoe kare ga
+// console.log(Math.floor(4.9));// is mn decimal ke baad ager koi bi no ho to wo round of nhn kare ga balke old no sho kare ga
+// console.log(Math.min(1,2,3,4,5,6));
+// console.log(Math.max(1,2,3,4,5,6));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min =10
+const max =20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
