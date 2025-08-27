073b88e5ae68ff76eae595739723a18d224e5cf6
ameermaviaamkj-create <ameermaviaamkj@gmail.com>
2025-08-27 08:50:05 +0000

    scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,38 @@
+// // var c = 300
+// let a = 300
+// if (true){
+//      let a = 10
+//      const b = 20
+//      console.log("Inner:",a);
+// }
+//  console.log(a);
+// //  console.log(b);
+// //  console.log(c);
+
+
+function one() {
+    const username = "Ameer Mavai"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    two()   
+}
+// one()
+
+if (true) {
+    const username = "Ameer Mavai"
+    if (username === "Ameer Mavia") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);   
+}
+// console.log(username);
+
+
+//++++++++++++++++++Intresting+++++++++++++++++++++++
+
+
